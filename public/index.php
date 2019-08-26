c41b475b5258a5fd7ad12ae30cd37528aaa093c2
Hasan Nawaz <hasan@hasan.es>
2019-08-26 15:06:03 +0200

    test fb sdk connection

--- public/index.php
+++ public/index.php
@@ -1,5 +1,23 @@
-<?php 
+<?php
 
-phpinfo();
+require_once __DIR__ . '/../vendor/autoload.php';
+use FacebookAds\Api;
+use FacebookAds\Logger\CurlLogger;
+use FacebookAds\Object\AdAccount;
+use FacebookAds\Object\Campaign;
+use FacebookAds\Object\Fields\CampaignFields;
 
-?>
+$app_id = "{your-app-id}";
+$app_secret = "{your-appsecret}";
+$access_token = "{your-access-token}";
+$account_id = "act_{your-adaccout-id}";
+
+Api::init($app_id, $app_secret, $access_token);
+
+$account = new AdAccount($account_id);
+$cursor = $account->getCampaigns();
+
+// Loop over objects
+foreach ($cursor as $campaign) {
+  echo $campaign->{CampaignFields::NAME}.PHP_EOL;
+}
